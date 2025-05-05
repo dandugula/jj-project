eb7a4c81e609223d039e80d4b7ec4dbdb154fb5b
Chaitanya Dandugula <chaitanya@chadan.in>
2025-05-05 13:15:44 +0530

    Refactor

--- hello.go
+++ hello.go
@@ -5,6 +5,14 @@ import "fmt"
 // An awesome way to print hello world in Go
 
 func main() {
+	print_hello()
+	print_goodbye()
+}
+
+func print_hello() {
 	fmt.Println("Hello World!")
+}
+
+func print_goodbye() {
 	fmt.Println("Goodbye World!")
 }
