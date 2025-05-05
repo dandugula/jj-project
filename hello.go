9d0bdb478cb36063a5dac3e297368ec5798110af
Chaitanya Dandugula <chaitanya@chadan.in>
2025-05-05 10:32:44 +0530

    Add initial files to the repo
    Add initial files
    
    print goodbye
    
    Add comments
    
    Refactor
    
    Add some more comments
    
    print goodbye

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// An awesome way to print hello world in Go
+
+func main() {
+	print_hello()
+	print_goodbye()
+}
+
+// Print hello
+func print_hello() {
+	fmt.Println("Hello World!")
+	fmt.Println("Goodbye World!")
+}
+
+// Print goodbye
+func print_goodbye() {
+	fmt.Println("Goodbye World!")
+}
