6df4c89ac91352cef92b70196d418bd890828d4a
Chaitanya Dandugula <chaitanya@chadan.in>
2025-05-06 08:52:36 +0530

    Add some more comments

--- hello.go
+++ hello.go
@@ -9,10 +9,12 @@ func main() {
 	print_goodbye()
 }
 
+// Print hello
 func print_hello() {
 	fmt.Println("Hello World!")
 }
 
+// Print goodbye
 func print_goodbye() {
 	fmt.Println("Goodbye World!")
 }
